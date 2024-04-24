f3280d3b142c5399f185f478be2b1bbc357205c6
s10257699 <s10257699@connect.np.edu.sg>
2024-04-24 20:31:52 +0800

    GIVE ME A TICK

--- app/src/main/java/sg/edu/np/mad/mad_prac2/User.java
+++ app/src/main/java/sg/edu/np/mad/mad_prac2/User.java
@@ -1,12 +1,12 @@
 package sg.edu.np.mad.mad_prac2;
 
-public class UserTest
+public class User
 {
     public String name;
     public String description;
     public int id;
     public boolean followed;
-    public UserTest(String name, String description, int id, Boolean followed)
+    public User(String name, String description, int id, Boolean followed)
     {
         this.name = name;
         this.description = description;
