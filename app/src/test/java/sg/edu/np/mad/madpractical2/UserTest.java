d4bc5f200ec24e1e687c148e12ec28f0f7bbe93c
s10257699 <s10257699@connect.np.edu.sg>
2024-04-26 08:14:15 +0800

    yolo

--- app/src/test/java/sg/edu/np/mad/madpractical2/UserTest.java
+++ app/src/test/java/sg/edu/np/mad/madpractical2/UserTest.java
@@ -9,22 +9,22 @@ public class UserTest {
     @Test
     public void testUserConstructor() {
         // Test the constructor of the User class
-        User user = new User("John Doe", "MAD Developer", 1, false);
+        User userTest = new User("John Doe", "MAD Developer", 1, false);
 
         // Assert that the user object is not null
-        assertNotNull(user);
+        assertNotNull(userTest);
 
         // Assert that the user's name matches the expected value
-        assertEquals("John Doe", user.name);
+        assertEquals("John Doe", userTest.name);
 
         // Assert that the user's description matches the expected value
-        assertEquals("MAD Developer", user.description);
+        assertEquals("MAD Developer", userTest.description);
 
         // Assert that the user's id matches the expected value
-        assertEquals(1, user.id);
+        assertEquals(1, userTest.id);
 
         // Assert that the user's followed status matches the expected value
-        assertFalse(user.followed);
+        assertFalse(userTest.followed);
     }
 
     // You can add more test methods here to cover other functionalities of the User class
